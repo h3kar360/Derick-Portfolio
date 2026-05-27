1ca64c52bb14150d7d31a51548f16f24d64c928c
Derick <dericklievian@gmail.com>
2026-05-27 22:56:03 +0800

    added an introduction component in the homepage and made navbar fixed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="w-full flex justify-between items-center text-2xl px-5 py-2">
+        <nav className="w-full flex justify-between items-center text-2xl px-5 py-2 fixed inset-x-0">
             <a href="#home">Derick A Lievian</a>
             <div className="flex justify-items-center items-center gap-5">
                 <a href="#skills">Skills</a>
