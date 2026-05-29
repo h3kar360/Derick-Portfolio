e1e500e1136e5bd310b00aef2591412b59611581
Derick <dericklievian@gmail.com>
2026-05-29 22:29:00 +0800

    fixed grammar and added resume download button in introduction section

--- frontend/src/components/HomePage/Introduction.tsx
+++ frontend/src/components/HomePage/Introduction.tsx
@@ -7,11 +7,18 @@ const Introduction = () => {
             <section className="hidden w-full h-screen md:flex justify-evenly items-center">
                 <div>
                     <p className="lg:text-6xl">Hi, I'm Derick</p>
-                    <p className="lg:text-2xl">
+                    <p className="lg:text-2xl mb-10">
                         A Computer Science student @CUHK-SZ
                         <br />
-                        Expected to graduate at 2029
+                        Expected to graduate in 2029
                     </p>
+                    <a
+                        href="/files/Resume-Derick.pdf"
+                        target="_blank"
+                        className="bg-green-400 lg:px-5 lg:py-3 rounded-xl text-2xl hover:bg-green-300"
+                    >
+                        Resume
+                    </a>
                 </div>
                 <img
                     src={derickPp}
@@ -29,11 +36,18 @@ const Introduction = () => {
                 />
                 <div>
                     <p className="text-4xl">Hi, I'm Derick</p>
-                    <p className="text-xl">
+                    <p className="text-xl mb-8">
                         A Computer Science student @CUHK-SZ
                         <br />
                         Expected to graduate at 2029
                     </p>
+                    <a
+                        href="/files/Resume-Derick.pdf"
+                        target="_blank"
+                        className="bg-green-400 px-3 py-1.5 text-2xl rounded-xl hover:bg-green-300"
+                    >
+                        Resume
+                    </a>
                 </div>
             </section>
         </section>
